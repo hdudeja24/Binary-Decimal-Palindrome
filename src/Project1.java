02ac63594916b50fd98d0d421306aef355a06b93
hdudeja24 <hitanshududeja80@gmail.com>
2020-09-26 17:35:02 -0400

    Ask and filter user input for two numbers.
    Overview:
    - Added a FilterInput function that splits the int array to the first two values in the line.
    In-Depth:
    - In the FilterInput function we output an array,  of size: 2, no matter how many inputs in the line.
    Test:
    - Tested by taking extra values in input but only accepting the needed ones

--- src/Project1.java
+++ src/Project1.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
 
 public class Project1 {
 	
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input Range > ");
+
+		String input = sc.nextLine();
+		int [] range = FilterInput(input);
+
+		for (Integer num: range)
+			System.out.println(num);
 	}
+
+	private static int [] FilterInput(String line){
+		/**
+		 * This function takes a string and splits it into an int array equivalent to the first two values in the line
+		 **/
+
+		int[] range = new int[2];							// This will be the array we will return
+		String nums[] = line.split(" ");				// Splitting the String into array
+
+		 for(int i = 0; i < range.length; i++)				// Loop to insert values into range
+		 	range[i] = Integer.parseInt(nums[i]);			// Inserting int value of String array
+
+		 return range;										// Return array
+	}
+
 }
