79999aea572afc50770a986a97963ff859add74a
hdudeja24 <hitanshududeja80@gmail.com>
2020-09-26 18:15:04 -0400

    Create Stack.java

---
+++ src/Stack.java
@@ -0,0 +1,26 @@
+public class Stack implements StackInterface {
+    @Override
+    public Object push(Object item) {
+        return null;
+    }
+
+    @Override
+    public Object pop(Object item) {
+        return null;
+    }
+
+    @Override
+    public Object peek(Object item) {
+        return null;
+    }
+
+    @Override
+    public boolean empty() {
+        return false;
+    }
+
+    @Override
+    public int search(Object item) {
+        return 0;
+    }
+}
